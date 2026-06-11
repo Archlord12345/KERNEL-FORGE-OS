5cec9e8f586cbdc18bb075ad1a1f18c43c9162e8
archlordroy-codeur <archlordroy@gmail.com>
2026-06-11 11:24:00 +0100

    fix: complete archiso profile structure and configuration

--- arch-build/profiledef.sh
+++ arch-build/profiledef.sh
@@ -1,15 +1,15 @@
 iso_name="kernel-forge-os"
 iso_label="KERNEL_FORGE_OS"
-iso_publisher="Kernel Forge"
-iso_version=$(date +%Y.%m.%d)
+iso_publisher="Archlord12345"
+iso_application="KERNEL-FORGE-OS"
+iso_version="1.0.0"
 install_dir="arch"
-buildmodes=('iso')
 bootmodes=('bios.syslinux' 'uefi.systemd-boot')
 arch="x86_64"
 pacman_conf="pacman.conf"
-airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '100%')
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1048576')
 file_permissions=(
-  ["/etc/shadow"]="0:0:400"
-  ["/usr/local/bin/customize_airootfs.sh"]="0:0:755"
+  ["/etc/shadow"]="0:0:0400"
+  ["/root"]="0:0:0700"
+  ["/usr/local/bin/customize_airootfs.sh"]="0:0:0755"
 )

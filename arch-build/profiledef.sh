4faaae511022abd73f26613389f8cf0756779af8
archlordroy-codeur <archlordroy@gmail.com>
2026-06-11 11:13:01 +0100

    fix: update archiso config to support multilib and non-deprecated boot modes

--- arch-build/profiledef.sh
+++ arch-build/profiledef.sh
@@ -4,12 +4,12 @@ iso_publisher="Kernel Forge"
 iso_version=$(date +%Y.%m.%d)
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux' 'uefi.systemd-boot')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '100%')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
-  ["/usr/bin/forge-setup"]="0:0:755"
+  ["/usr/local/bin/customize_airootfs.sh"]="0:0:755"
 )
